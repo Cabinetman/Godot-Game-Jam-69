3a87756e3e8505fb8f414a228e0ee6e11a4eccd0
cxprss <98793924+cxprss@users.noreply.github.com>
2024-05-11 18:27:17 -0400

    add mapping for up and down and fix player movement

--- test3/Player.gd
+++ test3/Player.gd
@@ -12,16 +12,17 @@ func _physics_process(delta):
 	# Add the gravity.
 	#if not is_on_floor():
 	#	velocity.y += gravity * delta
-
+	
 	# Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	var ydir = Input.get_axis("down","up")
-	print(ydir)
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left",'right')
+	var ydir = Input.get_axis("up","down")
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -31,5 +32,7 @@ func _physics_process(delta):
 	if ydir:
 		velocity.y = ydir * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	print(velocity)
+
 	move_and_slide()
